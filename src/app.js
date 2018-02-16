91f6c965e56671b30a19099018d16a1e1842a921
root <razasaif171@gmail.com>
2018-02-16 15:52:10 +0500

    boiler plate setting

--- src/app.js
+++ src/app.js
@@ -1,3 +1,14 @@
 import Rx from "rxjs/Rx";
-
-console.log("RxJS Boiler Running...");
+import $ from "jquery";
+var btn =$("#btn");
+var input =$("#input");
+const btnStream$=Rx.Observable.fromEvent(btn,'click');
+btnStream$.subscribe(function(e){
+    console.log(e.target.innerHTML);
+    
+})
+const inputStream$=Rx.Observable.fromEvent(input,'keypress');
+inputStream$.subscribe(function(e){
+    console.log(e.currentTarget.value);
+    
+})
